7001c0cd3d697d8309714f4c3447525109476af7
João Victor <joaovenus@gmail.com>
2021-05-21 15:36:17 -0300

    Add metodo update em barbearia e cliente

--- Codigo/backend/back-my-barber/src/controller/ClienteController.ts
+++ Codigo/backend/back-my-barber/src/controller/ClienteController.ts
@@ -15,5 +15,10 @@ export class ClienteController {
         const cliente = await repository.findOne({ where: {email} });
         return cliente;
     }
+    async findById( id: string) {
+        const cliente = await getManager().findOne(Cliente, id);
+        return cliente;
+
+    }
 }
 
