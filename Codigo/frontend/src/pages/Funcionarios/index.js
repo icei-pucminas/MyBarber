8e925f7bd2134d991ae46f485a37744fbc8e418f
Thiago Frois <zthiagopf@gmail.com>
2021-05-25 20:17:32 -0300

    integracao funcionarios

--- Codigo/frontend/src/pages/Funcionarios/index.js
+++ Codigo/frontend/src/pages/Funcionarios/index.js
@@ -1,15 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FiTrash2, FiEdit2 } from 'react-icons/fi';
-import fakeData from './fakeData';
+import api from '../../services/api';
 
 import { Container, CardFuncionario } from './styles';
 import Button from '../../components/Button';
 import ModalCadastro from './ModalCadastro';
 
+const DEFAULT_IMG = "https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png";
+
 const Funcionarios = () => {
 
+  const { id: idBarbearia } = JSON.parse(localStorage.getItem('user'));
+  const [employees, setEmployees] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [editingEmployee, setEditingEmployee] = useState({});
+
+  useEffect(() => {
+    async function getEmployees() {
+      try {
+        const { data } = await api.get(`/barbearia/funcionarios/${idBarbearia}`);
+        setEmployees(data);
+      } catch (error) {
+        console.log(error);
+        alert("Ocorreu um erro ao buscar os funcionários!");
+      }
+    }
+    getEmployees();
+  }, [idBarbearia])
 
   const handleEdit = (funcionario) => {
     setEditingEmployee(funcionario);
@@ -21,23 +38,30 @@ const Funcionarios = () => {
     setShowModal(false);
   }
 
-  const handleDelete = (funcionario) => {
-    console.log(funcionario);
+  const handleDelete = async ({ id }) => {
+    try {
+      await api.delete(`/funcionario/delete/${id}`);
+      const newEmployees = employees.filter((employee) => employee.id !== id);
+      setEmployees(newEmployees);
+    } catch (error) {
+      console.log(error);
+      alert("Ocorreu um erro ao excluir o funcionário.");
+    }
   }
 
   return (
     <>
-      <ModalCadastro show={showModal} handleClose={handleClose} funcionario={editingEmployee} />
+      <ModalCadastro show={showModal} handleClose={handleClose} funcionario={editingEmployee} idBarbearia={idBarbearia} />
       <Container blur={showModal}>
         <h1>Funcionários</h1>
-        {fakeData.map((funcionario) => (
+        {employees.map((funcionario) => (
           <CardFuncionario key={funcionario.nome}>
-            <img src={funcionario.foto} alt={funcionario.nome} />
+            <img src={DEFAULT_IMG} alt={funcionario.nome} />
             <main>
               <p>{funcionario.nome}</p>
               <section>
                 <span>{funcionario.telefone}</span>
-                <span>Horário de Trabalho: {funcionario.horaInicio} às {funcionario.horaFim}</span>
+                <span>Horário de Trabalho: {funcionario.horarioInicial} às {funcionario.horarioFinal}</span>
                 <div>
                   <button type="button" onClick={() => handleEdit(funcionario)}><FiEdit2 size={32} /></button>
                   <button type="button" onClick={() => handleDelete(funcionario)}><FiTrash2 size={32} /></button>
